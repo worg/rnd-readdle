949945980f1d3bb6169e91ed35ad3e1f1369e11b
Hiram J. Pérez <hi@worg.xyz>
2018-01-16 23:30:32 -0600

    posts categories

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -1,3 +1,15 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import PostList from '../posts/list';
 
-export default (props) => <div>CATEGORY {console.warn('PROPS', props)}</div>;
+const mapStateToProps = ({ posts }, { match }) => {
+  const { params: { category } } = match;
+  return {
+    posts: Object.values(posts.byId).filter(p => {
+      return p.category === category;
+    }),
+    title: category,
+    filter: category,
+  }
+};
+
+export default connect(mapStateToProps)(PostList)
