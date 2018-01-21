b5b33382aa8fcd840c7ef7cc2fe90cc6a53f99ac
Hiram J. Pérez <hi@worg.xyz>
2018-01-21 00:08:27 -0600

    basic post display, action buttons

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,3 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setEditModal } from '../../actions';
+import { OBJ } from '../../utils/constants';
 
-export default (props) => <div>POST {console.warn('PROPS', props)}</div>;
+import './post.css';
+
+export class Post extends Component {
+  handleEdit = () => {
+    const { post, editPost } = this.props;
+    editPost(post);
+  }
+
+  render() {
+    const { post } = this.props;
+    return (
+      <div className='post-detail'>
+        <div className='post-head'>
+          <h2 className='post-title'>{post.title}</h2>
+          <div className='action-items'>
+            <button
+              onClick={this.handleEdit}
+              title='edit post'>
+              <i className='fa fa-edit fa-fw' />
+            </button>
+            <button
+              onClick={()=>{}}
+              title='delete post'>
+              <i className='fa fa-trash fa-fw' />
+            </button>
+            <button
+              onClick={()=>{}}
+              title='upvote post'>
+              <i className='fa fa-thumbs-up fa-fw' />
+            </button>
+            <button title='upvote post'>
+              <i className='fa fa-thumbs-down fa-fw' />
+            </button>
+          </div>
+        </div>
+          <div className='post-body'>{post.body}</div>
+        <div className='post-comments'>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const { 
+    match: { params: { post: postId } },
+   } = ownProps;
+
+  return {
+    post: state.posts.byId[postId] || OBJ,
+  };
+};
+
+const mapDispatchToProps = {
+  editPost: setEditModal,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
